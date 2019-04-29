f6aea7f3ce3e579796f0f985082c8bde5057fc9f
IndraRamdhani <47512461+IndraRamdhani@users.noreply.github.com>
2019-04-29 10:52:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pesbuk</title>
+    <title>TOGE(Tour Guide Online)</title>
 
-     <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/index-baru.css">
 </head>
-<body class="bg-light">
-    <header>
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8">
-                        <h1>Selamat datang di Pesbuk</h1>
-                        <p>Bergabunglah bersama jutaan orang lainnya...</p>
-                    </div>
-                    <div class="col-md-4">
-                        <a href="login.php" class="btn btn-secondary">Masuk</a>
-                        <a href="register.php" class="btn btn-success">Daftar</a>
-                    </div>
-                </div>
-            </div>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+      <div class="container">
+          <img src="img/logo.png" width="100" height="70">
+      </div>
+    </nav>
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-10 align-self-end">
+          <h1 class="text-uppercase text-white font-weight-bold">TOGE(Tour Guide Online)</h1>
+          <hr class="divider my-4">
         </div>
-    </header>
-
-    <section>
-        <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <img class="img img-responsive" src="img/connect.png" />
-                    </div>
-                </div>
-            </div>
-    </section>
-
+        <div class="col-lg-8 align-self-baseline">
+          <p class="text-white-75 font-weight-light mb-5">selamat datang di web toge(tour guide online) dimana web ini akan membantu kalian untuk menemukan pemandu wisata ditempar kalian tuju, klik tombol LOGIN untuk kalian yang sudah memiliki akun dan klik tombol REGISTER untuk kalian yang pertama kali menggunakan jasa ini</p>
+          <a class="btn btn-primary btn-lg" href="login.php">LOGIN</a> <a class="btn btn-primary btn-lg" href="register.php">REGISTER</a>
+        </div>
+      </div>
+    </div>
+  </header>
 </body>
 </html>
