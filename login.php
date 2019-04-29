7a35c38e931f5bc94ad2e7d00c095d355bd82ef3
IndraRamdhani <47512461+IndraRamdhani@users.noreply.github.com>
2019-04-29 10:53:32 +0700

    Update login.php

--- login.php
+++ login.php
@@ -21,20 +21,21 @@ if(isset($_POST['login'])){
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
     // jika user terdaftar
+
     if($user){
-        // verifikasi password
-        if(password_verify($password, $user["password"])){
-            // buat Session
-            session_start();
-            $_SESSION["user"] = $user;
-            // login sukses, alihkan ke halaman timeline
-            header("Location: timeline.php");
+            // verifikasi password
+            if(password_verify($password, $user["password"])){
+                // buat Session
+                session_start();
+                $_SESSION["user"] = $user;
+                // login sukses, alihkan ke halaman timeline
+                header("Location: timeline.php");
+            }
+        else {
+          // login gagal, tambahin opsi daftar
+          echo "<p mb-4>login gagal, silakan daftar baru saja...</p>";
         }
-		else {
-			// login gagal, tambahin opsi daftar
-			echo "<p>login gagal, silakan daftar baru saja...</p>";
-		}
-    }
+        }
 }
 ?>
 
@@ -42,49 +43,34 @@ if(isset($_POST['login'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login Pesbuk</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-</head>
-<body class="bg-light">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-6">
+    <link rel="stylesheet" href="css/login.css"></head>
+<body>
 
-        <p>&larr; <a href="index.php">Home</a>
-
-        <h4>Masuk ke Pesbuk</h4>
-        <p>Belum punya akun? <a href="register.php">Daftar di sini</a></p>
-
-        <form action="" method="POST">
-
-            <div class="form-group">
-                <label for="username">Username</label>
-                <input class="form-control" type="text" name="username" placeholder="Username atau email" />
-            </div>
-
-
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input class="form-control" type="password" name="password" placeholder="Password" />
-            </div>
-
-            <input type="submit" class="btn btn-success btn-block" name="login" value="Masuk" />
-
-        </form>
-            
-        </div>
-
-        <div class="col-md-6">
-            <!-- isi dengan sesuatu di sini -->
-        </div>
-
-    </div>
+<div class="container">
+    <p>&larr; <a href="index.php">Home</a>
+    <p>Belum punya akun? <a href="register.php">Daftar di sini</a></p>
+    <form class="form-signin" action="" method="POST">
+      <img class="mb-4" src="img/logo.png" alt="" width="102" height="72">
+      <h1 class="h3 mb-3 mr-center font-weight-normal">LOGIN</h1>
+      <label for="inputEmail" class="sr-only">Username</label>
+      <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" name="password" class="form-control" placeholder="Password" required>
+      <button class="btn btn-lg btn-primary btn-block" type="submit" value="Masuk" name="login">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2019</p>
+    </form>
 </div>
+
+
+  
+  
+
     
 </body>
 </html>
